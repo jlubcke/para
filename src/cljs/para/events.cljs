3b6e948ab8fa25f75db5323485d410cad476ea6d
Johan Lübcke <johan.lubcke@trioptima.com>
2017-06-05 15:09:43 +0200

    With add

--- src/cljs/para/events.cljs
+++ src/cljs/para/events.cljs
@@ -6,3 +6,10 @@
  :initialize-db
  (fn  [_ _]
    db/default-db))
+
+(re-frame/reg-event-db
+ :new-spending
+ (fn [db [_ [spender amount]]]
+   (println (str spender))
+      (println (str db))
+   (assoc-in db [:spendings spender] amount)))
--- src/cljs/para/views.cljs
+++ src/cljs/para/views.cljs
@@ -1,19 +1,43 @@
 (ns para.views
-    (:require [re-frame.core :as re-frame]))
+    (:require
+       [reagent.core :as reagent]
+       [re-frame.core :as re-frame]))
+
+(defonce spender (reagent/atom "Foo"))
+(defonce amount (reagent/atom 17))
+
+(defn spending-form []
+  [:form
+     [:h2 "Name"]
+     [:input {
+       :value @spender
+       :on-change #(reset! spender (-> % .-target .-value))
+     }
+     ]
+     [:h2 "Spending"]
+     [:input {
+        :value @amount
+       :on-change #(reset! amount (-> % .-target .-value))
+
+     }]
+     [:submit {
+       :type "button"
+       :on-click #(re-frame/dispatch [:new-spending [@spender @amount]])
+      } "Add"]
+     
+  ])
 
 (defn main-panel []
   (let [spendings @(re-frame/subscribe [:spendings])]
    [:div
      [:div "Hello there from " @(re-frame/subscribe [:name])]
-     (println spendings)
-
      [:ul 
        (map 
          (fn [[k v]]
            ^{:key k} [:li k "--" v])
          spendings
        )]
-     [:input ]
+     (spending-form)
    ]
  )
 )
