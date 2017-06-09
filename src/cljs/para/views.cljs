3b5692412a96b36061afb8902022162c805beac4
Johan Lübcke <johan.lubcke@trioptima.com>
2017-06-09 17:10:53 +0200

    Cleanup

--- src/cljs/para/views.cljs
+++ src/cljs/para/views.cljs
@@ -3,38 +3,55 @@
        [re-frame.core :as re-frame]))
 
 (defn spending-form []
-  [:div
-     [:h2 "Name"]
-     [:input {
-        :value @(re-frame/subscribe [:name])
-        :on-change #(re-frame/dispatch [:name (-> % .-target .-value)])
-     }]
-     [:h2 "Spending"]
-     [:input {
-        :value @(re-frame/subscribe [:amount])
-        :on-change #(re-frame/dispatch [:amount (-> % .-target .-value)])
-     }]
-     [:submit {
-       :type "button"
-       :on-click #(re-frame/dispatch [:new-spending @(re-frame/subscribe [:name]) @(re-frame/subscribe [:amount])])
-      } "Add"]
-  ])
+  (let [name @(re-frame/subscribe [:name])
+        amount @(re-frame/subscribe [:amount])]
+    [:h2 "Spendings"]
+    [:table
+      [:tr
+       [:th "Name"]
+       [:th "Spending"]
+      ]
+      [:tr
+        [:td
+          [:input {
+            :value amount
+            :on-change #(re-frame/dispatch [:name (-> % .-target .-value)])
+          }]
+        ]
+        [:td
+          [:input {
+            :value amount
+            :on-change #(re-frame/dispatch [:amount (-> % .-target .-value)])
+          }]
+        ]
+      ]
+      [:tr
+        [:td
+          [:submit {
+            :type "button"
+            :on-click #(re-frame/dispatch [:new-spending name amount])
+          } "Add"]
+        ]
+      ]
+    ]
+  )
+)
+
+(defn row [[k v]]
+  ^{:key k}
+  [:tr 
+    [:td k]
+    [:td v] 
+    [:td [:a {:on-click #(re-frame/dispatch [:edit k])}"Edit"]]
+    [:td [:a {:on-click #(re-frame/dispatch [:delete k])}"Delete"]]
+  ]
+)
 
 (defn main-panel []
   (let [spendings @(re-frame/subscribe [:spendings])]
    [:div
-     [:div "Hello there from " @(re-frame/subscribe [:name])]
-     [:table 
-       (map 
-         (fn [[k v]]
-           ^{:key k} [:tr 
-             [:td k]
-             [:td v] 
-             [:td [:a {:on-click #(re-frame/dispatch [:edit k])}"Edit"]]
-             [:td [:a {:on-click #(re-frame/dispatch [:delete k])}"Delete"]]
-           ])
-         spendings
-       )]
+     [:table (map row spendings)]
+     [:div "Modifying" @(re-frame/subscribe [:name])]
      (spending-form)
    ]
  )
